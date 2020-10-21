05ce3da44f381fbefac4681b0a31bd766c78ea42
Loya Altangerel <luvsaa@amazon.com>
2020-10-20 21:09:46 -0700

    Complete reverseWords

--- src/leetcode/_151_ReverseWordsString.java
+++ src/leetcode/_151_ReverseWordsString.java
@@ -1,4 +1,17 @@
 package leetcode;
 
 public class _151_ReverseWordsString {
+    public String reverseWords(String s) {
+        String[] sA = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=sA.length-1; i>=0; i--){
+            if(sA[i].length() > 0){
+                sb.append(sA[i]);
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString().trim();
+    }
 }
